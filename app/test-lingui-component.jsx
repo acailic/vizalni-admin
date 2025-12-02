ce311ad96b769a019c127f63f3d2362e23456371
aleksandarilic <a.ilic@vegait.rs>
2025-12-02 01:01:09 +0100

    Remove unused Trans import in test component

--- app/test-lingui-component.jsx
+++ app/test-lingui-component.jsx
@@ -1,11 +1,10 @@
-import { Trans, t } from '@lingui/macro';
+import { t } from '@lingui/macro';
 
 export const TestComponent = () => {
   const message = t`Hello World`;
 
   return (
     <div>
-      <Trans id="test.greeting">Hello, this is a test</Trans>
       <p>{message}</p>
     </div>
   );
